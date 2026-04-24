291b333d0b01d816d1bdd009041692339ea47e07
BIBEK ROUT <bibekrout8108@gmail.com>
2026-04-24 10:03:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.19%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.py
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -2,23 +2,5 @@ class Solution(object):
     def furthestDistanceFromOrigin(self, moves):
         p=moves.count('L')
         q=moves.count('R')
-        dis=0
-        if p<q:
-            for i in moves:
-                if i=='_' or i=='R':
-                    dis=dis+1
-                else:
-                    dis=dis-1
-        elif p>q:
-            for i in moves:
-                if i=='_' or i=='L':
-                    dis=dis-1
-                else:
-                    dis=dis+1
-        else:
-            for i in moves:
-                if i=='_' or i=='R':
-                    dis=dis+1
-                else:
-                    dis=dis-1
-        return abs(dis)       
+        U=moves.count('_')
+        return abs(p-q) + U
